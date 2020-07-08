a918a4afda62a7504ff52b94c8be86be3a589f64
mattiacentini <61649390+mattia-centini@users.noreply.github.com>
2020-07-08 16:57:02 +0200

    added wheeldelta

--- index.js
+++ index.js
@@ -1,58 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   //
   const section = [...document.querySelectorAll("section")];
-  const container = document.querySelector(".container");
+  //const container = document.querySelector(".container");
 
-  // const parag = document.createElement("p");
-  // parag.innerText = "you have " + section.length + " section";
-  // section[0].appendChild(parag);
-
-  // const sect0 = section[0].getBoundingClientRect().top;
-  // const sect1 = section[1].getBoundingClientRect().top;
-  // const sect2 = section[2].getBoundingClientRect().top;
-  // const sect3 = section[3].getBoundingClientRect().top;
-  // console.log(sect3);
-
-  // section.forEach((item) => {
-  //   let top = item.getBoundingClientRect().top;
-  //   console.log(top);
-  // });
-
-  // function hello(elem) {
-  //   return elem.getBoundingClientRect().top;
-  // }
-
-  const measure = (element) => {
+  const measure = element => {
     const sectSize = element.getBoundingClientRect();
     const topValue = sectSize.top;
-    //const starPosition = window.pageYOffset;
-    //const distance = topValue - starPosition;
-    //console.log("distance is " + distance);
     const visibility = topValue >= 0;
-    console.log(visibility);
+    console.log("top is " + topValue);
+    console.log("visibility " + visibility);
     return visibility;
   };
 
   const currSect = section.findIndex(measure); //findindex
-  console.log(currSect);
+  console.log("this is" + currSect);
 
-  function delta(e) {
-    return e.deltaY > 0 ? 1 : -1;
-  }
+  // function delta(e) {
+  //   return e.deltaY > 0 ? 1 : -1;
+  // }
 
-  function anything() {
-    if (delta() == 1) {
-      console.log("true");
-      const current = currSect - 1;
+  let index = 0;
+  window.addEventListener("wheel", e => {
+    const delta = e.wheelDelta;
+    if (delta < 0) {
+      section[index].scrollIntoView({ behavior: "smooth", block: "start" });
     }
-  }
-
-  //get to current section using function or method(scrollIntoView)
-  //check if the lenght of the section is not longer than our section[array]
-  //create timeout that stops the crazy scroll
-  //The deltaY property returns a positive value when scrolling down, and a negative value when scrolling up, otherwise 0.
-
-  //console.log(section.findIndex(measure));
+  });
 
   //SCROLL WITH BUTTON
   const button = [...document.querySelectorAll(".button span")]; // turn node into array with spread operator
@@ -65,3 +38,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//get to current section using function or method(scrollIntoView)
+//check if the lenght of the section is not longer than our section[array]
+//create timeout that stops the crazy scroll
+
+//The deltaY property returns a positive (previous)value when scrolling down,
+//and a negative (negative) value when scrolling up, otherwise 0.
